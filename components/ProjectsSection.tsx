2cf8a4089c6feaf9e0cbf32415fb9bc006ea3d85
eryk <e.szczepaniec@lightpixel.io>
2026-02-09 14:45:00 +0100

    fear: add second project

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -15,15 +15,25 @@ export const ProjectsSection = () => {
         "Tailwind CSS",
         "Sanity - Headless CMS",
       ],
+      url: "https://farluner.com/",
+    },
+    {
+      id: 2,
+      title: "Simple Video Editor",
+      description: "Web App for video optimization & editing",
+      imgSrc: "/images/sve-web-app.webp",
+      techStack: ["Python", "Flask", "CSS", "HTML", "JavaScript"],
+      url: "https://www.simplevideoeditor.com/",
     },
   ];
   return (
     <div className="max-w-7xl mx-auto text-center">
       <h2 className="my-6 text-4xl">Commercial projects</h2>
-
-      {projectsData?.map?.((project) => (
-        <Project key={project.id} {...project} />
-      ))}
+      <div className="flex flex-wrap">
+        {projectsData?.map?.((project) => (
+          <Project key={project.id} {...project} />
+        ))}
+      </div>
     </div>
   );
 };
